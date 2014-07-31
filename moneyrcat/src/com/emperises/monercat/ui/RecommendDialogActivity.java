1a30769ce1fef8038e4318406ede2bcb49ebe6b2
haicuan139 <haicuan139@163.com>
2014-07-31 16:02:55 +0800

    调整应用图标，增加新的界面

--- moneyrcat/src/com/emperises/monercat/ui/RecommendDialogActivity.java
+++ moneyrcat/src/com/emperises/monercat/ui/RecommendDialogActivity.java
@@ -1,34 +1,36 @@
 package com.emperises.monercat.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.emperises.monercat.OtherBaseActivity;
 import com.emperises.monercat.R;
 
-public class ShenLingActivity extends OtherBaseActivity {
+public class RecommendDialogActivity extends OtherBaseActivity {
 
+	private Button mCloseButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_shenling);
-		setCurrentTitle("…Í¡Ïπ„∏Ê");
+		setContentView(R.layout.activity_recommend_dialog);
+		setTitleHide(View.GONE);
 	}
 	@Override
 	protected void initViews() {
-		super.initViews();
+		mCloseButton = (Button) findViewById(R.id.closeButton);
 	}
 	@Override
 	public void onClick(View v) {
 		super.onClick(v);
 		switch (v.getId()) {
-		case R.id.recommend_bt:
-			startActivity(new Intent(this , RecommendDialogActivity.class));
+		case R.id.closeButton:
+			finish();
 			break;
 
 		default:
 			break;
 		}
 	}
+
 }
